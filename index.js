04c5ee053a3f2b66d254801c1136f67029b2050a
Uros Ognjenovic <urosognjenovic17@gmail.com>
2025-02-06 22:42:13 +0100

    Refactoring

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ import { createPublicClient, http, getContract } from "viem";
 import { mainnet, base } from "viem/chains";
 import { MAINNET_STATE_VIEW_ADDRESS, BASE_STATE_VIEW_ADDRESS, STATE_VIEW_ABI } from "./constants.js";
 
-const mainnetPoolId = "0x21C67E77068DE97969BA93D4AAB21826D33CA12BB9F565D8496E8FDA8A82CA27";
-const basePoolId = "0x74B1EB0EB9068ED54B6B9D55673F7DE8FAC3299CE7E3DF916E0172676D225A1A";
-
 // Initialize the client
 const client = createPublicClient({
   chain: mainnet, 
@@ -54,6 +51,9 @@ const getPoolState = async (poolId) => {
 };
 
 const main = async() => {
+  const mainnetPoolId = "0x21C67E77068DE97969BA93D4AAB21826D33CA12BB9F565D8496E8FDA8A82CA27";
+  const basePoolId = "0x74B1EB0EB9068ED54B6B9D55673F7DE8FAC3299CE7E3DF916E0172676D225A1A";
+  
   const liquidity = await getPoolLiquidity(mainnetPoolId);
   console.log("liquidity:", liquidity);
 
@@ -61,7 +61,8 @@ const main = async() => {
     sqrtPriceX96,
     tick, 
     protocolFee, 
-    lpFee} = await getPoolState(mainnetPoolId);
+    lpFee
+  } = await getPoolState(mainnetPoolId);
 
   console.log(
     "sqrtPriceX96:", sqrtPriceX96,
