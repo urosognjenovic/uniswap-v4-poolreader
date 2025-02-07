61684c37c75a727b1e5758c7503c6277cb7b3db4
Uros Ognjenovic <urosognjenovic17@gmail.com>
2025-02-07 19:43:48 +0100

    Create getTickInfo

--- index.js
+++ index.js
@@ -100,6 +100,27 @@ const getTickLiquidity = async(poolId, tick) => {
   }
 }
 
+// Get the tick info
+const getTickInfo = async(poolId, tick) => {
+  try {
+    [
+      liquidityGross,
+      liquidityNet,
+      feeGrowthOutside0X128,
+      feeGrowthOutside1X128
+    ] = await stateView.read.getTickInfo(poolId, tick);
+
+    return {
+      liquidityGross,
+      liquidityNet,
+      feeGrowthInside0LastX128,
+      feeGrowthInside1LastX128
+    };
+  } catch(error) {
+    console.log("Error fetching tick info:", error);
+  }
+}
+
 const main = async() => {
   const mainnetPoolId = "0x21C67E77068DE97969BA93D4AAB21826D33CA12BB9F565D8496E8FDA8A82CA27";
   const basePoolId = "0x74B1EB0EB9068ED54B6B9D55673F7DE8FAC3299CE7E3DF916E0172676D225A1A";
