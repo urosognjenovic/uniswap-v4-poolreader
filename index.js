f3e545d9b9a6b1587482d5fc2718e8c17d42c8cb
Uros Ognjenovic <urosognjenovic17@gmail.com>
2025-02-08 11:44:56 +0100

    Create getTickBitmap

--- index.js
+++ index.js
@@ -155,6 +155,17 @@ const getFeeGrowthGlobals = async(poolId) => {
   }
 }
 
+// Get the tick bitmap of a pool at a specific tick
+const getTickBitmap = async(poolId, tick) => {
+  try {
+    const tickBitmap = await stateView.read.getTickBitmap(poolId, tick);
+
+    return tickBitmap;
+  } catch(error) {
+    console.log("Error fetching tick bitmap:", error);
+  }
+}
+
 const main = async() => {
   const mainnetPoolId = "0x21C67E77068DE97969BA93D4AAB21826D33CA12BB9F565D8496E8FDA8A82CA27";
   const basePoolId = "0x74B1EB0EB9068ED54B6B9D55673F7DE8FAC3299CE7E3DF916E0172676D225A1A";
